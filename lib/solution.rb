6cc82001cc6c1c90bb13e894e3dbf12b64c44728
JcFace <jcv@gmail.com>
2020-10-20 10:06:17 -0700

    We got it

--- lib/solution.rb
+++ lib/solution.rb
@@ -3,11 +3,11 @@ require 'pry'
 def clock_angle(time)
    arr = time.split(":")
    hour = (arr[0].to_f / 12.0) * 360.0
+   hour += (arr[1].to_f / 60) * 30
    minute = (arr[1].to_f / 60.0) * 360.0
-   if minute > hour 
-    hour = 360 + hour
-   end
+   hour = 0 if hour == 360
+   hour += 360 if minute > hour 
  hour - minute
 end
 
-Pry.start
+
